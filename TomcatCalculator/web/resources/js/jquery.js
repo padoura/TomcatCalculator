b39e82662c71cfe4e308f7eb2d371c27dea62a49
padoura <padoura@users.noreply.github.com>
2017-12-11 14:06:19 +0200

    Added calculator login in javascript-jquery

---
+++ TomcatCalculator/web/resources/js/jquery.js
@@ -0,0 +1,53 @@
+$("body").append($('<div> </div>'));
+
+$("div").append($('<p> </p>'));
+$("p").attr("id","p1");
+$("#p1").append($('<label>Number 1: </label>'));
+$("#p1").append($('<input type="text" value="2"> </input>'));
+$("#p1 > input").attr("id","number1");
+
+$("div").append($('<p></p>'));
+$("#p1").next().attr("id","p2");
+$("#p2").append($('<label>Number 2: </label>'));
+$("#p2").append($('<input type="text" value="2"> </input>'));
+$("#p2 > input").attr("id","number2");
+
+$("div").append($('<p></p>'));
+$("#p2").next().attr("id","p_sign");
+$("#p_sign").append($('<label>Sign: </label>'));
+$("#p_sign").append($('<input type="text"> </input>'));
+$("#p_sign > input").attr({"id":"sign","value":"**"});
+
+$("div").append($('<p></p>'));
+$("#p_sign").next().attr("id","p_calc");
+$("#p_calc").append($('<button>Calculate</button>'));
+$("button").attr("id","calc_button");
+
+$("div").append($('<p></p>'));
+$("#p_calc").next().attr("id","p_result");
+$("#p_result").append($('<label>Result:</label>'));
+$("#p_result").append($('<span> </span>'));
+$("#p_result span").attr("id","result");
+
+$("#sign").addClass("inputClass");
+$("#number1").addClass("inputClass");
+$("#number2").addClass("inputClass");
+$("#calc_button").addClass("inputClass");
+$("#result").addClass("inputClass");
+
+$("div").css({"display":"table"});
+$("p").css({"display":"table-row"});
+$("label").css({"display":"table-cell"});
+$(".inputClass").css({"display": "table-cell"});
+
+$("div").append('<button>Ajax</button>');
+
+$("body").append('<div id="div1"></div>');
+$("#div1").append('<button>This button will disappear</button>');
+
+
+$("#div1 > button").click(function(){
+    $.ajax({url: "http://echo.jsontest.com/key/value/one/two", success: function(result){
+        $("#div1").html(JSON.stringify(result));
+    }});
+});
